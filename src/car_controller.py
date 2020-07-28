ccbab7175cd4b5f4b1c34cfa92329c48a600ec55
tobidelbruck <tobi.1969>
2020-07-28 17:35:48 +0200

    - addd comment

--- src/car_controller.py
+++ src/car_controller.py
@@ -41,6 +41,7 @@ class car_controller:
         self.previous_waypoint = 0
 
     def read(self):
+        '''computes the control and returns it as a standard keyboard/joystick command'''
         waypoint_distance = self.car.track.get_distance_to_nearest_segment(car_state=self.car.car_state,
                                                                            x_car=self.car.car_state.position_m.x,
                                                                            y_car=self.car.car_state.position_m.y)
