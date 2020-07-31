7e379b5a74e0c11b6316ea7c480c0e4cc7f83629
Antonio Rios <jarios86@gmail.com>
2020-07-31 17:08:52 +0200

    Add comments to PID controller class and some modification in UPNP approach

--- src/l2race_utils.py
+++ src/l2race_utils.py
@@ -67,124 +67,43 @@ def video_writer(output_path, height, width, frame_rate=30):
             width, height))
     return out
 
+
 def open_ports():
     import upnpy
 
     upnp = upnpy.UPnP()
-
-    # Discover UPnP devices on the network
-    # Returns a list of devices e.g.: [Device <Broadcom ADSL Router>]
     devices = upnp.discover()
 
-    # Select the IGD
-    # alternatively you can select the device directly from the list
-    # device = devices[0]
-    device = upnp.get_igd()
+    for device in devices:
+        device.get_services()
+        service = device['WANIPConn1']
+        service.get_actions()
+        service.AddPortMapping.get_input_arguments()
+        s = CLIENT_PORT_RANGE.split('-')
+        if len(s) != 2:
+            raise RuntimeError(
+                'client port range {} should be of form start-end, e.g. 50100-50200'.format(portrange))
+        start_port = int(s[0])
+        end_port = int(s[1])
+        isbound = True
+        for p in range(start_port, end_port):
+            try:
+                # Finally, add the new port mapping to the IGD
+                # This specific action returns an empty dict: {}
+                service.AddPortMapping(
+                    NewRemoteHost='',
+                    NewExternalPort=p,
+                    NewProtocol='UDP',
+                    NewInternalPort=p,
+                    NewInternalClient='192.168.1.109', # this IP should be pass as an argument to client.py
+                    NewEnabled=1,
+                    NewPortMappingDescription='l2race mapping',
+                    NewLeaseDuration=0
+                )
+            except:
+                logger.warning('could not open port {}'.format(p))
+                isbound = False
+        if not isbound:
+            raise RuntimeError('could not bind to any port in range {}'.format(portrange))
 
-    # Get the services available for this device
-    # Returns a list of services available for the device
-    # e.g.: [<Service (WANPPPConnection) id="WANPPPConnection.1">, ...]
-    device.get_services()
-
-    # We can now access a specific service on the device by its ID
-    # The IDs for the services in this case contain illegal values so we can't access it by an attribute
-    # If the service ID didn't contain illegal values we could access it via an attribute like this:
-    # service = device.WANPPPConnection
-
-    # We will access it like a dictionary instead:
-    service = device['WANPPPConnection.1']
-
-    # Get the actions available for the service
-    # Returns a list of actions for the service:
-    #   [<Action name="SetConnectionType">,
-    #   <Action name="GetConnectionTypeInfo">,
-    #   <Action name="RequestConnection">,
-    #   <Action name="ForceTermination">,
-    #   <Action name="GetStatusInfo">,
-    #   <Action name="GetNATRSIPStatus">,
-    #   <Action name="GetGenericPortMappingEntry">,
-    #   <Action name="GetSpecificPortMappingEntry">,
-    #   <Action name="AddPortMapping">,
-    #   <Action name="DeletePortMapping">,
-    #   <Action name="GetExternalIPAddress">]
-    service.get_actions()
-
-    # The action we are looking for is the "AddPortMapping" action
-    # Lets see what arguments the action accepts
-    # Use the get_input_arguments() or get_output_arguments() method of the action
-    # for a list of input / output arguments.
-    # Example output of the get_input_arguments method for the "AddPortMapping" action
-    # Returns a dictionary:
-    # [
-    #     {
-    #         "name": "NewRemoteHost",
-    #         "data_type": "string",
-    #         "allowed_value_list": []
-    #     },
-    #     {
-    #         "name": "NewExternalPort",
-    #         "data_type": "ui2",
-    #         "allowed_value_list": []
-    #     },
-    #     {
-    #         "name": "NewProtocol",
-    #         "data_type": "string",
-    #         "allowed_value_list": [
-    #             "TCP",
-    #             "UDP"
-    #         ]
-    #     },
-    #     {
-    #         "name": "NewInternalPort",
-    #         "data_type": "ui2",
-    #         "allowed_value_list": []
-    #     },
-    #     {
-    #         "name": "NewInternalClient",
-    #         "data_type": "string",
-    #         "allowed_value_list": []
-    #     },
-    #     {
-    #         "name": "NewEnabled",
-    #         "data_type": "boolean",
-    #         "allowed_value_list": []
-    #     },
-    #     {
-    #         "name": "NewPortMappingDescription",
-    #         "data_type": "string",
-    #         "allowed_value_list": []
-    #     },
-    #     {
-    #         "name": "NewLeaseDuration",
-    #         "data_type": "ui4",
-    #         "allowed_value_list": []
-    #     }
-    # ]
-    service.AddPortMapping.get_input_arguments()
-
-    s = CLIENT_PORT_RANGE.split('-')
-    if len(s) != 2:
-        raise RuntimeError(
-            'client port range {} should be of form start-end, e.g. 50100-50200'.format(portrange))
-    start_port = int(s[0])
-    end_port = int(s[1])
-    isbound = False
-    for p in range(start_port, end_port):
-        try:
-            # Finally, add the new port mapping to the IGD
-            # This specific action returns an empty dict: {}
-            service.AddPortMapping(
-                NewRemoteHost='',
-                NewExternalPort=p,
-                NewProtocol='UDP',
-                NewInternalPort=p,
-                NewInternalClient='0.0.0.0',
-                NewEnabled=1,
-                NewPortMappingDescription='l2race mapping.',
-                NewLeaseDuration=3600
-            )
-        except:
-            logger.warning('could not open port {}'.format(p))
-    if not isbound:
-        raise RuntimeError('could not bind to any port in range {}'.format(portrange))
 
--- src/pid_next_waypoint_car_controller.py
+++ src/pid_next_waypoint_car_controller.py
@@ -10,7 +10,6 @@ class pid_next_waypoint_car_controller:
     """
     car controller: gets state and provides control signal.
     This reference implementation is a basic PID controller that aims for the next waypoint.
-
     """
     def __init__(self, my_car: car = None):
         """
@@ -22,36 +21,27 @@ class pid_next_waypoint_car_controller:
         self.car = my_car
         self.car_command = car_command()
 
-        # internet values kp=0.1 ki=0.001 kd=2.8
+        # PID constants
         self.steer_Kp = 0.08
         self.steer_Ki = 0.000015
         self.steer_Kd = 2.8
-
-        # internet values kp=0.45 ki=0.000 kd=0.5
-        self.thro_Kp = 0.45
-        self.thro_Ki = 0.001
-        self.thro_Kd = 1.5
 
         self.steer_p_error = 0
         self.steer_i_error = 0
         self.steer_d_error = 0
 
-        self.thro_p_error = 0
-        self.thro_i_error = 0
-        self.thro_d_error = 0
-
-        self.previous_waypoint = 0
-        self.angle = 0
-
+    # Computes the control and returns it as a standard keyboard/joystick command
     def read(self):
-        '''computes the control and returns it as a standard keyboard/joystick command'''
+        # Get the distance from the car position to the nearest waypoint (Middle of the road)
         waypoint_distance = self.car.track.get_distance_to_nearest_segment(car_state=self.car.car_state,
                                                                            x_car=self.car.car_state.position_m.x,
                                                                            y_car=self.car.car_state.position_m.y)
+        # Update the steering error using the distance from the car to the middle os the road
         self.__update_steering_error(cte=waypoint_distance)
-        self.__update_throttle_error(cte=abs(waypoint_distance))
-        self.car_command.throttle = 0.1 #self.output_throttle(1)
+        # Get the next steering angle and use it in the car command
         self.car_command.steering = self.output_steering_angle()
+        # For this example the throttle is fixed
+        self.car_command.throttle = 0.1
         return self.car_command
 
     def __update_steering_error(self, cte):
@@ -61,18 +51,8 @@ class pid_next_waypoint_car_controller:
         self.steer_i_error += cte
         self.steer_d_error = cte - pre_cte
 
-    def __update_throttle_error(self, cte):
-        pre_cte = self.thro_p_error
-
-        self.thro_p_error = cte
-        self.thro_i_error += cte
-        self.thro_d_error = cte - pre_cte
-
+    # Compute the steering angle using the error and the PID constants
     def output_steering_angle(self):
         angle = - self.steer_Kp * self.steer_p_error - self.steer_Ki * self.steer_i_error - \
                self.steer_Kd * self.steer_d_error
         return angle
-
-    def output_throttle(self, max_thro):
-        return max_thro - self.thro_Kp * self.thro_p_error - self.thro_Ki * self.thro_i_error - \
-               self.thro_Kd * self.thro_d_error
