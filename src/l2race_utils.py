cd24895c9a8f51b15d172bb2d2ed4d3ba76bec62
tobidelbruck <tobi.1969>
2020-08-05 20:54:38 +0200

    - added method to become daemon under linux for server, to go to background process.

--- src/l2race_utils.py
+++ src/l2race_utils.py
@@ -43,6 +43,49 @@ class loop_timer():
             else:
                 logger.warning('loop_timer cannot achieve desired rate {}Hz, time ran over by {}ms compared with allowed time {}ms'.format(self.rate_hz, -leftover_time*1000, max_sleep*1000))
 
+# https://stackoverflow.com/questions/1423345/can-i-run-a-python-script-as-a-service
+import os, sys
+
+def become_daemon(our_home_dir='.', out_log='/dev/null', err_log='/dev/null', pidfile='daemon.pid'):
+    """ Make the current process a daemon.  """
+
+    try:
+        # First fork
+        try:
+            if os.fork() > 0:
+                sys.exit(0)
+        except OSError as e:
+            logger.warning('Could not become daemon: fork #1 failed" (%d) %s\n' % (e.errno, e.strerror))
+            # sys.exit(1)
+
+        os.setsid()
+        os.chdir(our_home_dir)
+        os.umask(0)
+
+        # Second fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # You must write the pid file here.  After the exit()
+                # the pid variable is gone.
+                fpid = open(pidfile, 'wb')
+                fpid.write(str(pid))
+                fpid.close()
+                sys.exit(0)
+        except OSError as e:
+            logger.critical('Could not become daemon: fork #2 failed" (%d) %s\n' % (e.errno, e.strerror))
+            # sys.exit(1)
+
+        si = open('/dev/null', 'r')
+        so = open(out_log, 'a+', 0)
+        se = open(err_log, 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+    except Exception as e:
+        logger.warning('Could not become daemon (might only work under linux): {}'.format(e))
+
+
 def my_logger(name):
     logging.basicConfig(level=LOGGING_LEVEL)
     # root = logging.getLogger()
