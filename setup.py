f5cd76b754a0d0999c67f8add1dc2765e7b1cf00
tobidelbruck <tobi.1969>
2020-08-15 20:50:38 +0200

    - improve setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, setuptools
+from setuptools import setup, find_packages
 from Cython.Build import cythonize
 # run with
 # python setup.py build_ext --inplace
@@ -6,26 +6,23 @@ from Cython.Build import cythonize
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="l2race-tellurideneuromorphs", # Replace with your own username
-    version="0.0.1",
+setup(
+    name="l2race", # Replace with your own username
+    version="2.0.1",
     author="Marcin Paluch, Antonio Rios, Chang Gao, Tobi Delbruck",
-    author_email="tobi@ini.uzh.ch",
-    description="L2RACE challenge Telluride Neuromorphic workshop",
+    author_email="tobi@ini.uzh.ch,marcin.paluch1994@gmail.com,arios@us.es",
+    description="L2RACE challenge for Telluride Neuromorphic workshop",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/neuromorphs/l2race",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    scripts=['main.py','servery.py'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: CC License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
-)
-
-# needs to be run on server after touchinng any of the pyx files with "python setup.py build_ext --inplace"
-setup(
     ext_modules = cythonize([
         "commonroad/vehicleDynamics_MB.pyx",
         "commonroad/vehicleDynamics_ST.pyx",
@@ -40,5 +37,7 @@ setup(
         # "commonroad/parameters_vehicle1.py",
         # "commonroad/parameters_vehicle2.py",
         # "commonroad/parameters_vehicle3.py",
-        ], compiler_directives={'language_level' : "3"})
+    ], compiler_directives={'language_level' : "3"})
 )
+
+# needs to be run on server after touching any of the pyx files with "python setup.py build_ext --inplace"
