aa4b4fc6fd97852f02a95a101a131aa9d2d08ea5
Ante Marić <ante.maric@fer.hr>
2021-01-08 11:38:05 +0100

    changed sindy_KS to model state variables individually; updated SINDy KS model in models.py;added (unfinished) MPC controller for SINDy KS

--- modeling/SINDy/sindy_KS.py
+++ modeling/SINDy/sindy_KS.py
@@ -5,15 +5,18 @@ Built on PySINDy: https://github.com/dynamicslab/pysindy
 @author Ante Maric
 """
 
+import csv
 from pathlib import Path
 import numpy as np
 import pandas as pd
 import pysindy as ps
-# from functools import wraps
+from functools import wraps
+
+from scipy.interpolate import interp1d
 
 from random import randrange
 import matplotlib.pyplot as plt
-# from scipy.integrate import solve_ivp
+from scipy.integrate import solve_ivp
 
 
 class Data:
@@ -62,8 +65,8 @@ class Data:
 
                 data = data.drop_duplicates('time')
 
-                data['body_angle_deg'] = np.deg2rad(data['body_angle_deg'])
-                data['steering_angle_deg'] = np.deg2rad(data['steering_angle_deg'])
+                data['body_angle_rad'] = np.deg2rad(data['body_angle_deg'])
+                data['steering_angle_rad'] = np.deg2rad(data['steering_angle_deg'])
 
                 t_single = data['time']
                 u_single = data[COMMANDS]
@@ -119,64 +122,61 @@ class SINDy(ps.SINDy):
                         multiple_trajectories=data.multiple_trajectories,
                         unbias=True)
 
-    def plot(self, test_data, simulated_data, save=False):
+    def simulate_step(self, curr_state, curr_command, t, dt):
         """
-        plots trained model for evaluation
+        simulates one step forward from starting state and returns new state
+        (to be used with the ghost car feature)
         ...
 
         Parameters
         ----------
-        model: SINDy model object
-            trained SINDy model
-        test_data: Data object
-            test data object constructed using Data class
-            if test_data contains multiple trajectories, one will be randomly
-            selected for testing
-        save: bool
-            if set to True, the plot will be saved as .png
+        curr_state: src.car_state.car_state object
+            current (starting) state of the car
+        curr_command: src.car_state.car_command object
+            command passed to the car at starting time
+        t: np.float64
+            next timestep
+        dt: np.float64
+            next-current timestep difference
 
-        Plots
-        -----
-        car path (x,y) (ground truth vs. model simulation)
-        each feature specified in FEATURES with respect to time
-        (ground truth vs. model simulation)
+        Returns
+        -------
+        new_state: src.car_state.car_state object
+            simulated state at next timestep
         """
 
-        plt.rcParams.update({'font.size': 18})
+        # get values of all used commands
+        cmds = [getattr(curr_command, x) for x in self.commands]
 
-        _, axs = plt.subplots(1, 1, figsize=(13, 8))
+        def u(t):  # u has to be callable in order to work with pysindy
+            return np.array(cmds)
 
-        axs.plot(test_data.pos[:, 0], test_data.pos[:, 1], 'g+',
-                 label='true path')
-        axs.plot(simulated_data[:, 0], simulated_data[:, 1], 'r+',
-                 label='model')
-        axs.invert_yaxis()
-        axs.legend()
-        axs.set(title='Car path', xlabel=r'$x$', ylabel=r'$y$')
-        plt.tight_layout()
+        # get values of all used states
+        states = [getattr(curr_state, x) for x in self.features]
+        # stitch them into a starting state
+        s0 = np.concatenate([s if hasattr(s, '__iter__')
+                             else [s] for s in states])
 
-        if save:
-            plt.savefig('car_path.png')
+        sim = super().simulate(s0, [t-dt, t], u)
 
-        _, axs = plt.subplots(len(FEATURES), 1, figsize=(12, 18))
+        new_state = copy.copy(curr_state)
 
-        for i, feature in enumerate(FEATURES):
-            axs[i].plot(test_data.t, test_data.x[:, i], 'k',
-                        label='true data')
-            axs[i].plot(test_data.t, simulated_data[:, i+2], 'r--',
-                        label='model')
-            axs[i].legend()
-            axs[i].set(title=feature, xlabel=r'$t\ [s]$', ylabel=feature)
+        # construct new state
+        # solve case when feature of just one coordinate is used ?
+        # (e.g., vel.y, but not vel.x) ; probably not needed
+        i = 0
+        for f in self.features:
+            if f in ['position_m', 'velocity_m_per_sec', 'accel_m_per_sec_2']:
+                setattr(new_state, f, Vector2(sim[1, i], sim[1, i+1]))
+                i += 2
+            else:
+                setattr(new_state, f, sim[1, i])
+                i += 1
 
-        plt.tight_layout()
-
-        if save:
-            plt.savefig('features.png')
-
-        plt.show()
+        return new_state
 
 
-def solver_simulate(model, test_data):
+def solver_simulate(model_speed, model_steering, model_body_angle, test_data):
     """
     simulates test data from beginning to end using trained model
     simulation is done "step by step" - each timestep is simulated separately
@@ -200,7 +200,9 @@ def solver_simulate(model, test_data):
         resulting simulated data
     """
 
-    i = randrange(len(test_data.t))
+    print("Simulating ...")
+
+    i = randrange(0, len(test_data.t))
     test_data.x = test_data.x[i]
     test_data.pos = test_data.pos[i]
     test_data.x_dot = test_data.x_dot[i]
@@ -209,18 +211,17 @@ def solver_simulate(model, test_data):
 
     # use newer solver (multiple available methods):
     # def flipped_arguments(fun):
-    #    @wraps(fun)
-    #    def fun_flipped(x, y):
-    #        return fun(y, x)
-    #    return fun_flipped
+    #     @wraps(fun)
+    #     def fun_flipped(x, y):
+    #         return fun(y, x)
+    #     return fun_flipped
 
     # def solve_ivp_wrapped(fun, y0, t, *args, **kwargs):
-    #    return solve_ivp(flipped_arguments(fun), tuple([t[0], t[-1]]),
-    #                     y0, *args, method='Radau', dense_output=True,
-    #                     t_eval=t, atol=1e-4, rtol=1e-2, **kwargs).y.T
+    #     return solve_ivp(flipped_arguments(fun), tuple([t[0], t[-1]]),
+    #                      y0, *args, method='Radau', dense_output=True,
+    #                      t_eval=t, atol=1e-6, rtol=1e-4, **kwargs).y.T
 
-    def u(t):
-        return np.array(cmds)
+    u = lambda t: np.array(cmds)
 
     s0 = test_data.x[0]
     pos0 = test_data.pos[0]
@@ -229,27 +230,26 @@ def solver_simulate(model, test_data):
     for i in range(1, test_data.t.shape[0]):
         t = test_data.t[i]
         dt = t - test_data.t[i-1]
-        cmds = test_data.u[i-1]
-        sim = model.simulate(s0, [t-dt, t], u)
+        cmds = test_data.u[i-1, 0:2]
+        sim_speed = model_speed.simulate([s0[0]], [t-dt, t], u)
 
-        # --------------------------------------------------------------------
-        # newer solver
-        # sim = model.simulate(s0, [t-dt, t], u, integrator=solve_ivp_wrapped)
-        # --------------------------------------------------------------------
+        cmds = test_data.u[i-1, 2]
+        sim_steering = model_steering.simulate([s0[1]], [t-dt, t], u)
 
-        pos0[0] += s0[0]*np.cos(s0[2])*dt  # x
-        pos0[1] += s0[0]*np.sin(s0[2])*dt  # y
-        s0 = sim[1, :]
+        cmds = res[-1, 2:4]
+        sim_body_angle = model_body_angle.simulate([s0[2]], [t-dt, t], u)
+
+        pos0[0] += sim_speed[1]*np.cos(sim_body_angle[1])*dt  # x
+        pos0[1] += sim_speed[1]*np.sin(sim_body_angle[1])*dt  # y
+        s0 = np.hstack((sim_speed[1], sim_steering[1], sim_body_angle[1]))
         res = np.append(res, [np.hstack((pos0, s0))], axis=0)
 
     return test_data, res
 
 
-def euler_simulate(model, test_data):
+def plot(test_data, simulated_data, save=False):
     """
-    simulates test data from beginning to end using euler stepping
-
-    !!! WARNING: model is currently hard-coded in this function
+    plots trained model for evaluation
     ...
 
     Parameters
@@ -260,77 +260,126 @@ def euler_simulate(model, test_data):
         test data object constructed using Data class
         if test_data contains multiple trajectories, one will be randomly
         selected for testing
+    save: bool
+        if set to True, the plot will be saved as .png
 
-    Returns
-    -------
-    test_data: np.array() (temporary)
-        test data that was randomly chosen from test_dir
-    res: np.array()
-        resulting simulated data
+    Plots
+    -----
+    car path (x,y) (ground truth vs. model simulation)
+    each feature specified in FEATURES with respect to time
+    (ground truth vs. model simulation)
     """
-    t = test_data.t[0]
-    u = test_data.u[0]
-    pos = test_data.pos[0][0]
-    x = test_data.x[0][0]
-    res = np.array([np.hstack((pos, x))])
 
-    for i in range(1, t.shape[0]):
-        dt = t[i]-t[i-1]
+    print("Plotting ...")
 
-        d_speed = 2.473*u[i-1, 0] * dt  # * 2.81/2.473
+    plt.rcParams.update({'font.size': 18})
 
-        d_steer = 1.552 * (np.abs((0.5*u[i-1, 1]-res[i-1, 3])) > np.deg2rad(1)) \
-                        * np.sign(0.5 * u[i-1, 1] - res[i-1, 3]) * dt  # * 2/1.552
+    _, axs = plt.subplots(1, 1, figsize=(13, 8))
 
-        d_angle = 0.383 * res[i-1, 2] * np.tan(res[i-1, 3]) * dt  # * 0.38776/0.383
-        d_x = res[i-1, 2] * np.cos(res[i-1, 4]) * dt
-        d_y = res[i-1, 2] * np.sin(res[i-1, 4]) * dt
+    axs.plot(test_data.pos[:, 0], test_data.pos[:, 1], 'g+',
+                label='true path')
+    axs.plot(simulated_data[:, 0], simulated_data[:, 1], 'r+',
+                label='model')
+    axs.invert_yaxis()
+    axs.legend()
+    axs.set(title='Car path', xlabel=r'$x$', ylabel=r'$y$')
+    plt.tight_layout()
 
-        new = np.array([res[i-1, 0] + d_x,
-                        res[i-1, 1] + d_y,
-                        res[i-1, 2] + d_speed,
-                        res[i-1, 3] + d_steer,
-                        res[i-1, 4] + d_angle])
+    if save:
+        plt.savefig('car_path.png')
 
-        res = np.vstack((res, new))
+    _, axs = plt.subplots(len(FEATURES), 1, figsize=(12, 18))
 
-    test_data.x = test_data.x[0]
-    test_data.pos = test_data.pos[0]
-    test_data.x_dot = test_data.x_dot[0]
-    test_data.u = test_data.u[0]
-    test_data.t = test_data.t[0]
+    for i, feature in enumerate(FEATURES):
+        axs[i].plot(test_data.t, test_data.x[:, i], 'k',
+                    label='true data')
+        axs[i].plot(test_data.t, simulated_data[:, i+2], 'r--',
+                    label='model')
+        axs[i].legend()
+        axs[i].set(xlabel=r'$t\ [s]$', ylabel=feature)
 
-    return test_data, res
+    plt.tight_layout()
 
+    if save:
+        plt.savefig('features.png')
+
+    plt.show()
 
 if __name__ == '__main__':
+
+    # NOTE: in this script, seperate models are created for each of the state variables
+    # this is the easiest way to get the satisfactory coefficients, although a bit unrealistic for real case scenarios
+    # for a single-model implementation, take a look at past versions of this script on github
+    # results are similar and can likely be improved for both implementations
 
     train_dir = 'Train'
     test_dir = 'Test'
 
-    FEATURES = [
-                'speed_m_per_sec',
-                'steering_angle_deg',
-                'body_angle_deg'
-                ]
+    # STEERING_MAX = 0.5  # can be checked in simulator
 
-    COMMANDS = [
-                'command.throttle',
-                'command.steering',
-                # 'command.brake'
-                ]
+    # FIT SPEED MODEL
+    FEATURES = ['speed_m_per_sec']
+    COMMANDS = ['command.throttle', 'command.brake']
+    PRECALCULATED_DERIVATIVES = []
 
+    library_functions = [lambda x: x]
+    library_function_names = [lambda x: x]
+
+    custom_library = ps.CustomLibrary(
+        library_functions=library_functions,
+        function_names=library_function_names,
+    )
+
+    model_speed = SINDy(
+        features=FEATURES,
+        commands=COMMANDS,
+        feature_library=custom_library,
+        optimizer=ps.STLSQ(threshold=2.5)
+        )
+
+    train_data = Data(train_dir)
+    model_speed.fit(train_data)
+    model_speed.print()
+
+    # FIT STEERING MODEL
+    FEATURES = ['steering_angle_rad']
+    COMMANDS = ['command.steering']
+    PRECALCULATED_DERIVATIVES = []
+
+    # library_functions = [
+    #     lambda x, y: (np.abs(STEERING_MAX*y-x) > np.deg2rad(1)) * np.sign(STEERING_MAX*y-x),
+    # ]
+
+    # library_function_names = [
+    #     lambda x, y: '(|{}*y-x| > radians(1)) * sign({} * '.format(STEERING_MAX, STEERING_MAX) + y + ' - ' + x + ')',
+    # ]
+
+    # custom_library = ps.CustomLibrary(
+    #     library_functions=library_functions,
+    #     function_names=library_function_names,
+    # )
+
+    model_steering = SINDy(
+        features=FEATURES,
+        commands=COMMANDS,
+        feature_library=ps.PolynomialLibrary(degree=1),  # custom_library
+        optimizer=ps.STLSQ(threshold=2)
+        )
+
+    train_data = Data(train_dir)
+    model_steering.fit(train_data)
+    model_steering.print()
+
+    # FIT BODY ANGLE MODEL
+    FEATURES = ['body_angle_rad']
+    COMMANDS = ['speed_m_per_sec', 'steering_angle_rad']
     PRECALCULATED_DERIVATIVES = []
 
     library_functions = [
-        lambda x: x,
-        lambda x, y: (np.abs(0.5*y-x) > np.deg2rad(1)) * np.sign(0.5*y-x),
         lambda x, y: x*np.tan(y)
     ]
 
     library_function_names = [
-        lambda x: x,
-        lambda x, y: '(|0.5*y-x| > radians(1)) * sign(0.5 * ' + y + ' - ' + x + ')',
         lambda x, y: x + '*' + 'tan(' + y + ')'
     ]
 
@@ -339,22 +388,37 @@ if __name__ == '__main__':
         function_names=library_function_names,
     )
 
-    model = SINDy(
+    model_body_angle = SINDy(
         features=FEATURES,
         commands=COMMANDS,
         feature_library=custom_library,
-        optimizer=ps.SR3(threshold=0.18, nu=0.0003, tol=1e-5, thresholder='l1',
-                         fit_intercept=False, normalize=False, max_iter=100000),
-        differentiation_method=ps.FiniteDifference(order=1)
+        optimizer=ps.STLSQ()
         )
 
     train_data = Data(train_dir)
-    model.fit(train_data)
-    model.print()
+    model_body_angle.fit(train_data)
+    model_body_angle.print()
 
+    # PLOT ENTIRE MODEL ON TEST DATA
+    FEATURES = ['speed_m_per_sec', 'steering_angle_rad', 'body_angle_rad']
+    COMMANDS = ['command.throttle', 'command.brake', 'command.steering']
     test_data = Data(test_dir)
+    test_data, simulated_data = solver_simulate(model_speed,
+                                                model_steering,
+                                                model_body_angle,
+                                                test_data)
+    plot(test_data, simulated_data)
 
-    test_data, sim = euler_simulate(model, test_data)
-    # test_data, sim = solver_simulate(model, test_data)
+    # coefficients
+    # speed_coef = model_speed.optimizer.coef_
+    # steering_coef = model_steering.optimizer.coef_
+    # body_angle_coef = model_body_angle.optimizer.coef_
 
-    model.plot(test_data, sim, save=False)
+    # SAVE MODEL TO CSV
+    print('Saving model ...')
+    precision = 3
+    with open('modeling/SINDy/KS_model.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(model_speed.equations(precision))
+        writer.writerow(model_steering.equations(precision))
+        writer.writerow(model_body_angle.equations(precision))
