1d50b11eaa93d1d999f1d5f624295cb1675073c2
Ante <ante.maric@fer.hr>
2022-02-23 19:53:42 +0100

    Modified testing scripts for multi-input systems

--- SI_Toolkit_ApplicationSpecificFiles/predictors_customization.py
+++ SI_Toolkit_ApplicationSpecificFiles/predictors_customization.py
@@ -1,10 +1,19 @@
 import numpy as np
+import yaml, os # TODO revamp this script, maybe remove altogether
 
-STATE_INDICES = []
-STATE_VARIABLES = []
-CONTROL_INPUTS = []
+config = yaml.load(open(os.path.join('SI_Toolkit_ApplicationSpecificFiles', 'config_training.yml'), 'r'), Loader=yaml.FullLoader)
+
+STATE_VARIABLES = config['training_default']['state_inputs']
+STATE_INDICES = {k: v for v, k in enumerate(STATE_VARIABLES)}
+CONTROL_INPUTS = config['training_default']['control_inputs']
 
 def augment_predictor_output(output_array, net_info):
+
+    pass
+
+    return output_array
+
+def next_state_predictor_ODE(dt, intermediate_steps):
 
     pass
 
